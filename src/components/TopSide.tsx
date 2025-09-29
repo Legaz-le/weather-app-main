9e9318c4880fecd7d88c92232e012972a583594f
legaz <188694281+Legaz-le@users.noreply.github.com>
2025-09-29 17:44:57 +0900

    added button and search styling

--- src/components/TopSide.tsx
+++ src/components/TopSide.tsx
@@ -1,13 +1,14 @@
 import logo from "../public/images/logo.svg";
 import icon from "../public/images/icon-units.svg";
 import dropdown from "../public/images/icon-dropdown.svg";
+import search from "../public/images/icon-search.svg";
 
 export const TopSide = () => {
   return (
     <div className="flex flex-col w-full">
       <div className="flex items-center justify-between">
         <img src={logo} alt="logo-icon" />
-        <button className="flex gap-2 px-4 py-2 bg-Neutral-600 rounded cursor-pointer">
+        <button className="flex gap-2 px-4 py-2 bg-Neutral-600 rounded-lg cursor-pointer">
           <img src={icon} alt="icon-units" />
           Units
           <img src={dropdown} alt="icon-dropdrown" />
@@ -15,13 +16,18 @@ export const TopSide = () => {
       </div>
       <div className="flex flex-col items-center justify-center mt-15">
         <h1 className="text-5xl">How's the sky looking today?</h1>
-        <div className="flex items-center justify-between mt-10">
-          <input
-            type="search"
-            placeholder="Search for the place.."
-            className="bg-Neutral-300 p-2 "
-          />
-          <button>Search</button>
+        <div className="flex items-center justify-between mt-10 gap-2">
+          <div className="flex bg-Neutral-600 p-2 rounded-lg px-5 max-w-md gap-2 items-center">
+            <img src={search} alt="search-icon" />
+            <input
+              type="search"
+              placeholder="Search for the place.."
+              className=" w-md border-none bg-transparent text-white focus:outline-none "
+            />
+          </div>
+          <button className="px-4 py-2 bg-Neutral-600 rounded-lg cursor-pointer">
+            Search
+          </button>
         </div>
       </div>
     </div>
