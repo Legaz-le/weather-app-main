f7a63f116efec4d4150ce54f8dee341d627a4878
legaz <188694281+Legaz-le@users.noreply.github.com>
2025-10-15 20:36:18 +0900

    added drop down for days button

--- src/components/BodyElements/SideTable.tsx
+++ src/components/BodyElements/SideTable.tsx
@@ -10,10 +10,19 @@ export const SideTable = () => {
         <button
           className="px-4 py-2 flex flex-row  gap-3 bg-[#3C3B5E] rounded-lg
           cursor-pointer transition duration-300 focus:outline-none 
-             focus:ring-1 focus:ring-Neutral-0 focus:ring-offset-1"
+             focus:ring-1 focus:ring-Neutral-0 focus:ring-offset-1 relative"
         >
           <p className="font-DM-Sans xl:text-[16px] font-[500]">Tuesday</p>
           <img src={dropdown} alt="icon-dropDrown" />
+          <div className="absolute top-full  bg-Neutral-800 rounded-xl flex flex-col right-0 mt-2 p-2 gap-1 border-inline z-10 text-start w-[214px]">
+            <p className="px-2 py-2.5 flex flex-row  rounded-lg gap-2.5 bg-Neutral-700 font-[500] text-[16px] font-DM-Sans">Monday</p>
+            <p className="px-2 py-2.5 flex flex-row  rounded-lg gap-2.5 font-[500] text-[16px] font-DM-Sans">Tuesday</p>
+            <p className="px-2 py-2.5 flex flex-row  rounded-lg gap-2.5  font-[500] text-[16px] font-DM-Sans">Wednesday</p>
+            <p className="px-2 py-2.5 flex flex-row  rounded-lg gap-2.5  font-[500] text-[16px] font-DM-Sans">Thursday</p>
+            <p className="px-2 py-2.5 flex flex-row  rounded-lg gap-2.5  font-[500] text-[16px] font-DM-Sans">Friday</p>
+            <p className="px-2 py-2.5 flex flex-row  rounded-lg gap-2.5  font-[500] text-[16px] font-DM-Sans">Saturday</p>
+            <p className="px-2 py-2.5 flex flex-row  rounded-lg gap-2.5  font-[500] text-[16px] font-DM-Sans">Sunday</p>
+          </div>
         </button>
       </div>
       {HourlyForecastData.map((data) => (
